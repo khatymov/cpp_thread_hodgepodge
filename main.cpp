c761e5152485df82407c1d03f086c90e5f9702fa
Rene <khatymovrenat@gmail.com>
2023-11-13 02:14:56 +0300

    Add read-write functions

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <filesystem>
 #include <thread>
 #include "read_thread.h"
+#include <fstream>
 
 using namespace std;
 
@@ -27,6 +28,89 @@ namespace fs = std::filesystem;
 
 void read_file(const string_view& source_file)
 {
+    ifstream read_file(source_file.data());
+
+    if (!read_file.is_open())
+    {
+        throw std::runtime_error("Can't open source file");
+    }
+
+    // Read the file content line by line
+    std::string line;
+    while (std::getline(read_file, line)) {
+        std::cout << line << std::endl;
+    }
+
+    read_file.close();
+}
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+mutex _mutex;
+condition_variable cv;
+
+class StringBuffer {
+public:
+    void read(const string& val) {
+        unique_lock<mutex> lock(_mutex);
+        while (!q.empty()) {
+            cv.wait(lock);
+        }
+        q.push(val);
+        cv.notify_all();
+        cout << "Reader reads: " <<  val << endl;
+    }
+
+    auto write() {
+        unique_lock<mutex> lock(_mutex);
+        while (q.empty()) {
+            cv.wait(lock);
+        }
+        const string val = q.front();
+        q.pop();
+        cv.notify_all();
+        cout << "Writer writes " <<  val << endl;
+        return val;
+    }
+private:
+    queue<string> q;
+};
+
+
+void reader(StringBuffer* string_buffer, const string_view& source_file) {
+
+    ifstream read_file(source_file.data());
+
+    if (!read_file.is_open())
+    {
+        throw std::runtime_error("Can't open source file");
+    }
+
+    // Read the file content line by line
+    std::string line;
+    while (std::getline(read_file, line)) {
+        std::cout << line << std::endl;
+        string_buffer->read(line);
+    }
+
+    read_file.close();
+}
+
+void writer(StringBuffer *string_buffer, const string_view& dist_file) {
+
+    //TODO: verify that ostream creates a file
+    // Open the file for writing
+    std::ofstream write_file(dist_file.data());
+    if (!write_file.is_open())
+    {
+        throw std::runtime_error("Can't open destination file");
+    }
+
+    write_file << string_buffer->write();
+
+    write_file.close();
 }
 
 int main(int argc, char* argv[])
@@ -52,7 +136,9 @@ int main(int argc, char* argv[])
         return EXIT_FAILURE;
     }
 
-    std::jthread read_thread(read_file, source_path);
+    StringBuffer stringBuffer;
+    std::jthread read_thread(reader, &stringBuffer, source_path);
+    std::jthread write_thread(writer, &stringBuffer, target_path);
 
     return 0;
 }
